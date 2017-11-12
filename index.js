d29a29daeef755c1a6d2bcef94aa2becb958f243
Sahand Shokouhi <sahand.shok@gmail.com>
2017-11-12 21:38:09 +0330

    4

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+app.use(express.static());
 app.get('*', (req, res, next) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
 });
