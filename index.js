83d50a86c6564082b908ccc0b713819265d74d8a
Sahand Shokouhi <sahand.shok@gmail.com>
2017-11-12 22:10:00 +0330

    6

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const pug = require('pug');
 
 const app = express();
+
+fs.readdirSync('.', (err, files) => {
+  if (err) {throw err;}
+  console.log(files);
+});
 app.use(express.static(__dirname));
-app.get('*', (req, res, next) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'));
+app.set('view engine', 'pug');
+app.get('/', (req, res, next) => {
+  fs.readdir(path.join(__dirname, 'P90X3'), (err, files) => {
+    console.log(files);
+    files = files
+      .map(name => `<a href="http://172.104.145.38/P90X3/${name}">${name}</a>`);
+    return res.render('index', {files});
+  });
 });
 app.listen(80, () => console.log('Express server running'));
